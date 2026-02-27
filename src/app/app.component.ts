b556956f9849eee526cb92ca67738e1dbbed753f
Marco Comolli <marcoauron@hotmail.it>
2026-02-27 22:11:32 +0100

    Animazioni e logo e style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,9 +16,11 @@ export class AppComponent {
   public response: string = 'Ciao! Come prima cosa, inserisci il tuo nome:';
 
   private instruction_base: string = 'Forza, inserisci il tuo nome:';
-  private instruction: string = "";
+  private instruction: string = '';
   public status: Status = new Status();
   private statusKey: string = 'celi-status-key';
+
+  public isCssAnimating = false;
 
   constructor() {
     var storageItem = localStorage.getItem(this.statusKey);
@@ -29,6 +31,8 @@ export class AppComponent {
   }
 
   public onNameSubmit() {
+    this.startCssAnimation();
+
     let insertedInput = this.username;
     if (this.username === '') {
       return;
@@ -39,58 +43,75 @@ export class AppComponent {
     this.status.loginTry++;
 
     if (this.status.loginTry >= 5) {
-      this.instruction = `<br>(Nel caso non si fosse ancora capito..questa è una prova in sè..hai ancora ${this.status.loginRequiredTries - this.status.loginTry} tentativi per superarla)</br>` + this.instruction_base
+      this.instruction =
+        `<br>(Nel caso non si fosse ancora capito..questa è una prova in sè..hai ancora ${this.status.loginRequiredTries - this.status.loginTry} tentativi per superarla)</br>` +
+        this.instruction_base;
     }
 
     if (this.status.loginTry >= this.status.loginRequiredTries - 1) {
-      this.instruction = `<br>Ok, non ci stiamo capendo...provo a rendertelo un pochino più esplicito.. (hai un ultimo tentativo)</br> Perfavore, inserisci <span class="highlight">il tuo nome</span>:`
+      this.instruction = `<br>Ok, non ci stiamo capendo...provo a rendertelo un pochino più esplicito.. (hai un ultimo tentativo)</br> Perfavore, inserisci <span class="highlight">il tuo nome</span>:`;
     }
 
     if (this.status.loginTry >= this.status.loginRequiredTries) {
-      this.instruction = `<br>Eh vabbè, la prima prova non è andata benissimo, ma non ti scoraggiare e prova comunque ad entrare, cerchiamo di rendere più esplicito cosa vogliamo.</br> Perfavore, inserisci <span class="highlight2">il tuo nome</span>:`
+      this.instruction = `<br>Eh vabbè, la prima prova non è andata benissimo, ma non ti scoraggiare e prova comunque ad entrare, cerchiamo di rendere più esplicito cosa vogliamo.</br> Perfavore, inserisci <span class="highlight2">il tuo nome</span>:`;
     }
 
     if (this.status.loginTry >= this.status.loginRequiredTries + 1) {
-      this.instruction = `<br>Non ci siamo. Più di così non so come dirtelo</br> Perfavore, inserisci <span class="highlight3">il tuo nome</span> nella casella di testo qua sotto:`
+      this.instruction = `<br>Non ci siamo. Più di così non so come dirtelo</br> Perfavore, inserisci <span class="highlight3">il tuo nome</span> nella casella di testo qua sotto:`;
     }
 
     if (insertedInput.trim().startsWith('simone')) {
       this.status.enableUppercase = true;
-      this.response = `Oh suvvia, <b>${insertedInput}</b>, con la minuscola iniziale? Dai, puoi sforzarti di più..<br>${this.instruction}`;
+      this.response = `Oh suvvia, <span class="highlight2">${insertedInput}</span>, con la minuscola iniziale? Dai, puoi sforzarti di più..<br>${this.instruction}`;
       this.saveStatus();
       return;
     }
 
     if (toChecK.includes('parola') && toChecK.includes('simone')) {
-      this.response = `<b>${insertedInput}</b>. Nome e cognome nonostante io ti abbia chiesto di inserire il tuo nome. Non ci siamo.<br>${this.instruction}`;
+      this.response = `<span class="highlight2">${insertedInput}</span>. Nome e cognome nonostante io ti abbia chiesto di inserire il tuo nome. Non ci siamo.<br>${this.instruction}`;
       this.saveStatus();
       return;
     }
     if (insertedInput.trim().startsWith('Simone')) {
-      this.response = `Oh bene, <b>${insertedInput}</b>, con la lettera maiuscola. Così va meglio =). Tuttavia mi spiace dirti che non è la risposta corretta :(<br>${this.instruction}`;
+      this.response = `Oh bene, <span class="highlight2">${insertedInput}</span>, con la lettera maiuscola. Così va meglio =). Tuttavia mi spiace dirti che non è la risposta corretta<br>${this.instruction}`;
       this.saveStatus();
       return;
     }
 
     if (toChecK.startsWith('vysor')) {
-      this.response = `<b>${insertedInput}</b>! Ottimo tentativo! Ok che è il tuo nickname ma non è comunque quello che ti ho chiesto.<br>${this.instruction}`;
+      this.response = `<span class="highlight2">${insertedInput}</span>! Ottimo tentativo! Ok che è il tuo nickname ma non è comunque quello che ti ho chiesto.<br>${this.instruction}`;
       this.saveStatus();
       return;
     }
 
-    if (toChecK === "il tuo nome") {
+    if (toChecK.startsWith('marco')) {
+      this.response = `<span class="highlight2">${insertedInput}</span> :D. Ahah addirittura! Hai beccato chi ha fatto il sito ma no, hai sbagliato. <br>${this.instruction}`;
+      this.saveStatus();
+      return;
+    }
+
+    if (toChecK === 'il tuo nome') {
       this.status.phase = 1;
       this.saveStatus();
       return;
     }
 
-    this.response = this.instruction;
+    this.response = `Hai inserito <span class="highlight2">${insertedInput}</span> ma ahimè non è la risposta corretta..<br>${this.instruction}`;
     this.saveStatus();
   }
 
   private saveStatus() {
     localStorage.setItem(this.statusKey, JSON.stringify(this.status));
   }
+
+  private startCssAnimation() {
+    this.isCssAnimating = true;
+  }
+
+  public onPhase0End() {
+          this.status.phase = 1;
+      this.saveStatus();
+  }
 }
 
-export enum MainPhase { }
+export enum MainPhase {}
